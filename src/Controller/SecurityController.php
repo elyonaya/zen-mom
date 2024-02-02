0f2fea1ac6da43486b873ed4da158d9adfce521a
amel fradj <amelfradj1@gmail.com>
2024-02-02 10:55:12 +0100

    tf

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,7 +17,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('account/account.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
