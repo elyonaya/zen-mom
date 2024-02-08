d7fe9ef9bd7fa699d19c13af3487eb28f517042c
amel fradj <amelfradj1@gmail.com>
2024-02-08 15:47:17 +0100

    update

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,73 +7,71 @@ use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            
             ->add('email')
             ->add('emailConfirmation', EmailType::class, [
                 'constraints' => [
-                    new Assert\EqualTo([
-                        'propertyPath' => 'email',
-                        'message' => 'Les adresses email ne correspondent pas.'
-                    ])
+                    new Assert\Email([
+                        'message' => 'Veuillez entrer une adresse email valide.',
+                    ]),
+                    new Assert\Callback([$this, 'validateEmailConfirmation']),
                 ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
-                    new IsTrue([
+                    new Assert\IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // Au lieu d'être défini directement sur l'objet,
-                // cela est lu et encodé dans le contrôleur
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
+                    new Assert\NotBlank([
                         'message' => 'Entrer un mot de passe valide',
                     ]),
-                    new Length([
+                    new Assert\Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères.',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
             ])
             ->add('plainPasswordConfirmation', PasswordType::class, [
-                'mapped' => false, // Ne pas mapper cet attribut à l'entité utilisateur
-                'constraints' => [
-                    new Assert\EqualTo([
-                        'propertyPath' => 'plainPassword',
-                        'message' => 'Les mots de passe ne correspondent pas.'
-                    ])
-                ]
-            ])
-        ;
+                'mapped' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            // Configure your form options here
         ]);
+    }
+
+    public function validateEmailConfirmation($emailConfirmation, ExecutionContextInterface $context)
+    {
+        $email = $context->getRoot()['email']->getData();
+
+        if ($emailConfirmation !== $email) {
+            $context->buildViolation('Les adresses email ne correspondent pas.')
+                ->atPath('emailConfirmation')
+                ->addViolation();
+        }
     }
 }
